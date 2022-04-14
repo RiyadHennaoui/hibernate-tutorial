3543c38d99a3d365cee6d3a6d36b0a7d1294e6e0
Riyad Hennaoui <riyad.hennaoui@gmail.com>
2022-04-14 14:08:05 +0200

    Read done

--- src/main/java/fr/afpa/controller/ConnectionHibernate.java
+++ src/main/java/fr/afpa/controller/ConnectionHibernate.java
@@ -5,6 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class ConnectionHibernate {
 
     public static void main(String[] args) {
@@ -20,14 +22,52 @@ public class ConnectionHibernate {
             Student tempStudent2 = new Student("Mike", "Doe", "mike@domain.com");
             Student tempStudent3 = new Student("Bonita", "Applebaum", "bonita@domain.com");
 
+            //create students in database
             session.beginTransaction();
-            session.save(tempStudent1);
-            session.save(tempStudent2);
-            session.save(tempStudent3);
+//            session.save(tempStudent1);
+//            session.save(tempStudent2);
+//            session.save(tempStudent3);
+
             session.getTransaction().commit();
+
+            //Read student in database
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Student thisStudent = session.get(Student.class, tempStudent1.getId());
+            System.out.println(thisStudent);
+            session.getTransaction().commit();
+
+            //Read with Query
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+
+            List<Student> students = session.createQuery("from Student").getResultList();
+
+            displayStudents(students);
+
+            students = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
+
+            displayStudents(students);
+            students = session.createQuery("from Student s where s.lastName='Doe' OR s.firstName='Pauleza'").getResultList();
+
+            displayStudents(students);
+
+            students = session.createQuery("from Student s where s.email LIKE '%domain.com'").getResultList();
+
+            displayStudents(students);
+
+            session.getTransaction().commit();
+
 
         }finally {
             factory.close();
         }
+    }
+
+    private static void displayStudents(List<Student> students) {
+        students.forEach(System.out::println);
     }
 }
